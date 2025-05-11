f2045cfeb8b7fb030711085e16eda674b2bf0fc6
lDenji1994 <david1sierra2003@gmail.com>
2025-05-11 17:26:40 -0500

    implementacion de menu seleccion de operacion

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+print(" 1- Suma. \n 2- Resta. \n 3- Multiplicacion. \n 4 - Divicion.")
+entrada = int(input("Bienvenido porfavor seleccione una opcion "))
+
+if entrada == 1:
+    print("1")
+if entrada == 2:
+    print("2")
+if entrada == 3:
+    print("3")
+if entrada == 4:
+    print("4")
