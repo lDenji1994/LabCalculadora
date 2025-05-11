ff69e16231d8085f13c0935b40fff4896e556482
lDenji1994 <david1sierra2003@gmail.com>
2025-05-11 17:37:51 -0500

    implementacion de la operacion de suma

--- calculadora.py
+++ calculadora.py
@@ -1,11 +1,20 @@
 print(" 1- Suma. \n 2- Resta. \n 3- Multiplicacion. \n 4 - Divicion.")
 entrada = int(input("Bienvenido porfavor seleccione una opcion "))
 
-if entrada == 1:
-    print("1")
-if entrada == 2:
-    print("2")
-if entrada == 3:
-    print("3")
-if entrada == 4:
-    print("4")
+while entrada != 0:
+    entrada = int(input("Bienvenido porfavor seleccione una opcion "))
+    if entrada == 1:
+        entrada2 = int(input("Desea seguir sumando (si 1 : no 0): "))
+        while entrada2 !=0: 
+            a = int(input("ingrese el primer numero: "))
+            b = int(input("ingrese el segundo numero: "))
+
+            suma = a + b
+            print("Resultado de la suma: " , suma)
+            entrada2 = int(input("Desea seguir sumando (si 1 : no 0): "))
+    if entrada == 2:
+        print("2")
+    if entrada == 3:
+        print("3")
+    if entrada == 4:
+        print("4")
